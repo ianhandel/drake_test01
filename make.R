b72faf6842eebc111440d159dba9068342f06b92
ianhandel <ianhandel@hotmail.com>
2020-05-05 08:17:15 +0100

    alter order

--- make.R
+++ make.R
@@ -1,9 +1,10 @@
 library(drake)
 
 source("R/packages.R")
-source("R/plan.R")
 source("R/plot_cars.R")
 source("R/summarise_cars.R")
 source("R/regress_cars.R")
+
+source("R/plan.R")
 
 make(ians_analysis,lock_envir = FALSE) #gt package needs this as modifies global .env
